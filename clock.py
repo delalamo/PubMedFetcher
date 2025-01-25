54a9010251a86c0f4321f7b9cf538765b82096b6
delalamo <diego.delalamo@gmail.com>
2025-01-25 07:33:34 +0100

    SMTP works!

--- clock.py
+++ clock.py
@@ -1,20 +1,14 @@
-from email.mime.text import MIMEText
 from datetime import datetime
 import os
 from dotenv import load_dotenv
 from apscheduler.schedulers.blocking import BlockingScheduler
-import requests
 
-# from urllib2 import Request, urlopen
-import urllib.request
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 # Load environment variables
 load_dotenv()
-
-# Email configuration (replace with your actual details)
-# SENDER_EMAIL = os.environ.get("SENDER_EMAIL")
-# SENDER_PASSWORD = os.environ.get("SENDER_PASSWORD")
-# RECEIVER_EMAIL = "123@test.com"
 
 sched = BlockingScheduler()
 
@@ -22,21 +16,22 @@ sched = BlockingScheduler()
 @sched.scheduled_job("interval", minutes=1)
 def send_email():
     """Sends an email with the current time."""
+
+    print(os.environ.get("MY_EMAIL"), os.environ.get("MY_PW"))
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     message = MIMEText(f"The current time is: {now}")
 
-    request = urllib.request.Request(
-        "https://realemail.expeditedaddons.com/?api_key="
-        + os.environ["REALEMAIL_API_KEY"]
-        + "&email=diego.delalamo%40gmail.com&fix_typos=false"
-    )
+    message = MIMEMultipart()
+    message["From"] = os.environ.get("MY_EMAIL")
+    message["To"] = os.environ.get("MY_EMAIL")
+    message["Subject"] = "TEST TEST"
+    message.attach(MIMEText("TEST TEST TEST ", "plain"))
 
-    response_body = urllib.request.urlopen(request).read()
-    print(response_body)
-
-    # print(message)
-    # response = requests.request("POST", url, headers=headers, data=payload)
-    # print(response.json())
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(os.environ.get("MY_EMAIL"), os.environ.get("MY_PW"))
+        server.sendmail(
+            os.environ.get("MY_EMAIL"), os.environ.get("MY_EMAIL"), message.as_string()
+        )
 
 
 sched.start()
